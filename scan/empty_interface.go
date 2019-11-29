d1b84f89d42661bee6c3f8a478fa184fa388c386
Yann Salaün <yann.salaun@ulule.com>
2019-11-29 15:09:33 +0100

    scan: add SliceSlice

---
+++ scan/empty_interface.go
@@ -0,0 +1,69 @@
+package scan
+
+import "database/sql"
+
+// MapSlice returns rows scanned into a []map[string]interface{} value.
+func MapSlice(rows *sql.Rows) ([]map[string]interface{}, error) {
+	columns, err := rows.Columns()
+	if err != nil {
+		return nil, err
+	}
+
+	scannedrows, err := scanRows(columns, rows)
+	if err != nil {
+		return nil, err
+	}
+
+	maps := make([]map[string]interface{}, 0, len(scannedrows))
+	for _, row := range scannedrows {
+		values := make(map[string]interface{}, len(row))
+		for i, ptr := range row {
+			values[columns[i]] = *ptr.(*interface{})
+		}
+		maps = append(maps, values)
+	}
+	return maps, nil
+}
+
+// SliceSlice returns rows scanned into a [][]interface{} value.
+func SliceSlice(rows *sql.Rows) ([][]interface{}, error) {
+	columns, err := rows.Columns()
+	if err != nil {
+		return nil, err
+	}
+
+	scannedrows, err := scanRows(columns, rows)
+	if err != nil {
+		return nil, err
+	}
+
+	slices := make([][]interface{}, 0, len(scannedrows))
+	for _, row := range scannedrows {
+		values := make([]interface{}, len(row))
+		for i, ptr := range row {
+			values[i] = *ptr.(*interface{})
+		}
+		slices = append(slices, values)
+	}
+	return slices, nil
+}
+
+func scanRows(columns []string, rows *sql.Rows) ([][]interface{}, error) {
+	var scannedrows [][]interface{}
+	for rows.Next() {
+		var scannedptrs []interface{}
+		for range columns {
+			scannedptrs = append(scannedptrs, new(interface{}))
+		}
+
+		if err := rows.Scan(scannedptrs...); err != nil {
+			return nil, err
+		}
+
+		scannedrows = append(scannedrows, scannedptrs)
+	}
+	if err := rows.Err(); err != nil {
+		return nil, err
+	}
+	return scannedrows, nil
+}
--- scan/empty_interface_test.go
+++ scan/empty_interface_test.go
@@ -62,3 +62,59 @@ func TestMapSlice(t *testing.T) {
 		t.Errorf(`expected nil, got %s`, m["null"])
 	}
 }
+
+func TestSliceSlice(t *testing.T) {
+	now := time.Now()
+	db := sql.OpenDB(&mockConnector{conn: &mockConn{stmt: &mockStmt{rows: &mockRows{
+		columns: []string{"int", "string", "time", "null"},
+		values: [][]driver.Value{
+			{1, "hello", now, nil},
+			{2, "world", now.Add(time.Second), nil},
+		},
+	}}}})
+	rows, err := db.Query("")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	slices, err := SliceSlice(rows)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if len(slices) != 2 {
+		t.Errorf("expected 2 rows, got %d", len(slices))
+	}
+	s := slices[0]
+	if len(s) != 4 {
+		t.Errorf("expected 4 columns, got %d", len(s))
+	}
+	if s[0] != 1 {
+		t.Errorf("expected 1, got %d", s[0])
+	}
+	if s[1] != "hello" {
+		t.Errorf(`expected "hello", got %s`, s[1])
+	}
+	if s[2] != now {
+		t.Errorf(`expected now, got %s`, s[2])
+	}
+	if s[3] != nil {
+		t.Errorf(`expected nil, got %s`, s[3])
+	}
+
+	s = slices[1]
+	if len(s) != 4 {
+		t.Errorf("expected 4 columns, got %d", len(s))
+	}
+	if s[0] != 2 {
+		t.Errorf("expected 2, got %d", s[0])
+	}
+	if s[1] != "world" {
+		t.Errorf(`expected "world", got %s`, s[1])
+	}
+	if s[2] != now.Add(time.Second) {
+		t.Errorf(`expected now, got %s`, s[2])
+	}
+	if s[3] != nil {
+		t.Errorf(`expected nil, got %s`, s[3])
+	}
+}
