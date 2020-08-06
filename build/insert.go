92ae9a2e28501da1f3ba2595ee65e27baf6a771b
Yann Salaün <yann.salaun@ulule.com>
2020-08-06 12:46:42 +0200

    build: add ON CONFLICT clause

--- build/insert.go
+++ build/insert.go
@@ -1,5 +1,7 @@
 package build
 
+import "fmt"
+
 // InsertInto builds a new INSERT command.
 func InsertInto(table string) *InsertCmd {
 	return &InsertCmd{table: Ident(table)}
@@ -8,6 +10,18 @@ func InsertInto(table string) *InsertCmd {
 // Values adds a VALUES clause.
 func (cmd *InsertCmd) Values(values ...ColumnValue) *InsertCmd {
 	cmd.values = values
+	return cmd
+}
+
+// OnConflict adds a ON CONFLICT clause.
+func (cmd *InsertCmd) OnConflict(action ConflictAction) *InsertCmd {
+	cmd.onconflict = &onconflictexpr{action: action}
+	return cmd
+}
+
+// OnConflictTarget adds a ON CONFLICT clause with a conflict target.
+func (cmd *InsertCmd) OnConflictTarget(target string, action ConflictAction) *InsertCmd {
+	cmd.onconflict = &onconflictexpr{target: Ident(target), action: action}
 	return cmd
 }
 
@@ -46,6 +60,12 @@ func (cmd *InsertCmd) build(b *builder) {
 		}
 		b.write(")")
 	}
+
+	if cmd.onconflict != nil {
+		b.write(" ")
+		cmd.onconflict.build(b)
+	}
+
 	if cmd.returning != nil {
 		b.write(" RETURNING ")
 		cmd.returning.build(b)
@@ -54,9 +74,10 @@ func (cmd *InsertCmd) build(b *builder) {
 
 // A InsertCmd is a INSERT command.
 type InsertCmd struct {
-	table     Expression
-	values    []ColumnValue
-	returning selectexprs
+	table      Expression
+	values     []ColumnValue
+	onconflict *onconflictexpr
+	returning  selectexprs
 }
 
 func Value(column string, value Expression) ColumnValue {
@@ -70,3 +91,61 @@ type ColumnValue struct {
 	column Expression
 	value  Expression
 }
+
+type onconflictexpr struct {
+	target Expression
+	action ConflictAction
+}
+
+func (e onconflictexpr) build(b *builder) {
+	b.write("ON CONFLICT ")
+	if e.target != nil {
+		b.write("(")
+		e.target.build(b)
+		b.write(") ")
+	}
+	e.action.build(b)
+}
+
+// A ConflictAction is a conflict action.
+type ConflictAction struct {
+	do     conflictactiondo
+	values []ColumnValue
+}
+
+func (a ConflictAction) build(b *builder) {
+	switch do := a.do; do {
+	case donothing:
+		b.write("DO NOTHING")
+	case doupdateset:
+		b.write("DO UPDATE SET ")
+		for i := range a.values {
+			if i > 0 {
+				b.write(", ")
+			}
+			a.values[i].column.build(b)
+			b.write(" = ")
+			a.values[i].value.build(b)
+		}
+	default:
+		panic(fmt.Sprintf("unknown conflict action %d", do))
+	}
+}
+
+// DoNothing is the DO NOTHING conflict_action.
+var DoNothing = ConflictAction{do: donothing}
+
+// DoUpdateSet is the DO UPDATE SET conflict_action.
+func DoUpdateSet(values ...ColumnValue) ConflictAction {
+	return ConflictAction{
+		do:     doupdateset,
+		values: values,
+	}
+}
+
+type conflictactiondo int
+
+const (
+	donothing conflictactiondo = iota
+	doupdateset
+)
--- build/insert_test.go
+++ build/insert_test.go
@@ -27,6 +27,36 @@ func TestInsert(t *testing.T) {
 			Returning(Columns("one", "two", "three")...),
 		out:  `INSERT INTO "table" ("foo", "bar") VALUES ($1, $2) RETURNING "one", "two", "three"`,
 		args: []interface{}{"hello", 1},
+	}, {
+		cmd: InsertInto("table").
+			Values(
+				Value("foo", Bind("hello")),
+				Value("bar", Bind(1)),
+			).
+			OnConflict(DoNothing),
+		out:  `INSERT INTO "table" ("foo", "bar") VALUES ($1, $2) ON CONFLICT DO NOTHING`,
+		args: []interface{}{"hello", 1},
+	}, {
+		cmd: InsertInto("table").
+			Values(
+				Value("foo", Bind("hello")),
+				Value("bar", Bind(1)),
+			).
+			OnConflictTarget("target", DoNothing),
+		out:  `INSERT INTO "table" ("foo", "bar") VALUES ($1, $2) ON CONFLICT ("target") DO NOTHING`,
+		args: []interface{}{"hello", 1},
+	}, {
+		cmd: InsertInto("table").
+			Values(
+				Value("foo", Bind("hello")),
+				Value("bar", Bind(1)),
+			).
+			OnConflict(DoUpdateSet(
+				Value("foo", Bind("hello")),
+				Value("bar", Bind(1)),
+			)),
+		out:  `INSERT INTO "table" ("foo", "bar") VALUES ($1, $2) ON CONFLICT DO UPDATE SET "foo" = $3, "bar" = $4`,
+		args: []interface{}{"hello", 1, "hello", 1},
 	}} {
 		t.Run(tt.out, func(t *testing.T) {
 			out, args := tt.cmd.Build()
