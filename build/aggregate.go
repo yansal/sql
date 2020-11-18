0d427d840847c0c6cf9fc19b796f116648b4f4da
Yann Salaün <yann@ulule.com>
2020-11-18 23:37:11 +0100

    build: add DISTINCT and ORDER BY to aggregate expressions

--- build/aggregate.go
+++ build/aggregate.go
@@ -1,18 +1,40 @@
 package build
 
 // Aggr returns a new AggrExpression.
-func Aggr(expr Expression) AggrExpression {
-	return aggrExpression{expr: expr}
+func Aggr(name string, exprs ...Expression) AggrExpression {
+	return aggrExpression{
+		name:  name,
+		exprs: exprs,
+	}
+}
+
+// AggrDistinct returns a new AggrExpression with the DISTINCT keyword.
+func AggrDistinct(name string, exprs ...Expression) AggrExpression {
+	return aggrExpression{
+		name:     name,
+		distinct: true,
+		exprs:    exprs,
+	}
 }
 
 // An AggrExpression is an aggregate expression.
 type AggrExpression interface {
+	Expression
+	OrderBy(Expression) AggrExpression
 	FilterWhere(Expression) Expression
 }
 
 type aggrExpression struct {
-	expr        Expression
+	name        string
+	distinct    bool
+	exprs       []Expression
+	orderby     Expression
 	filterwhere Expression
+}
+
+func (a aggrExpression) OrderBy(expr Expression) AggrExpression {
+	a.orderby = expr
+	return a
 }
 
 func (a aggrExpression) FilterWhere(expr Expression) Expression {
@@ -21,7 +43,22 @@ func (a aggrExpression) FilterWhere(expr Expression) Expression {
 }
 
 func (a aggrExpression) build(b *builder) {
-	a.expr.build(b)
+	b.write(a.name)
+	b.write("(")
+	if a.distinct {
+		b.write("DISTINCT ")
+	}
+	for i, expr := range a.exprs {
+		if i > 0 {
+			b.write(", ")
+		}
+		expr.build(b)
+	}
+	if a.orderby != nil {
+		b.write(" ORDER BY ")
+		a.orderby.build(b)
+	}
+	b.write(")")
 	if a.filterwhere != nil {
 		b.write(" FILTER (WHERE ")
 		a.filterwhere.build(b)
--- build/aggregate_test.go
+++ build/aggregate_test.go
@@ -13,11 +13,18 @@ func TestAggr(t *testing.T) {
 				CallExpr("count", Star),
 			).As("unfiltered"),
 			ColumnExpr(
-				Aggr(CallExpr("count", Star)).FilterWhere(Ident("i").LessThan(Int(5))),
+				Aggr("count", Star).FilterWhere(Ident("i").LessThan(Int(5))),
 			).As("filtered"),
 		).
 			From(FromExpr(CallExpr("generate_series", Int(1), Int(10))).As("i")),
 		out: `SELECT count(*) AS "unfiltered", count(*) FILTER (WHERE "i" < 5) AS "filtered" FROM generate_series(1, 10) AS "i"`,
+	}, {
+		stmt: Select(AggrDistinct("count", Ident("foo"))).
+			From(Ident("bar")),
+		out: `SELECT count(DISTINCT "foo") FROM "bar"`}, {
+		stmt: Select(Aggr("array_agg", Ident("a")).OrderBy(Order(Ident("b"), Desc))).
+			From(Ident("table")),
+		out: `SELECT array_agg("a" ORDER BY "b" DESC) FROM "table"`,
 	}} {
 		t.Run(tt.out, func(t *testing.T) {
 			out, args := tt.stmt.Build()
