2cc1ab6293c2fb3bb7d8fb38d52c93fa57777a77
Yann Salaün <yann.salaun@ulule.com>
2021-02-18 14:18:39 +0100

    nest: fix tests

--- nest/nest_test.go
+++ nest/nest_test.go
@@ -24,6 +24,9 @@ func (c *mockConn) Prepare(string) (driver.Stmt, error) { return nil, nil }
 func (c *mockConn) ExecContext(context.Context, string, []driver.NamedValue) (driver.Result, error) {
 	return nil, nil
 }
+func (c *mockConn) BeginTx(ctx context.Context, opts driver.TxOptions) (driver.Tx, error) {
+	return c.tx, nil
+}
 
 type mockTx struct{}
 
@@ -39,14 +42,23 @@ var (
 
 func TestRollback(t *testing.T) {
 	var (
-		begins  []hooks.BeginInfo
-		commits []hooks.CommitInfo
-		execs   []hooks.ExecInfo
+		begins  int
+		commits int
+		execs   []string
 	)
 	conn := hooks.Wrap(&mockConnector{conn: &mockConn{tx: &mockTx{}}})
-	conn.BeginHook = func(ctx context.Context, info hooks.BeginInfo) { begins = append(begins, info) }
-	conn.CommitHook = func(info hooks.CommitInfo) { commits = append(commits, info) }
-	conn.ExecHook = func(ctx context.Context, info hooks.ExecInfo) { execs = append(execs, info) }
+	conn.BeforeBegin = func(ctx context.Context, opts driver.TxOptions) context.Context {
+		begins++
+		return ctx
+	}
+	conn.BeforeCommit = func(ctx context.Context) context.Context {
+		commits++
+		return ctx
+	}
+	conn.BeforeExec = func(ctx context.Context, query string, args []driver.NamedValue) context.Context {
+		execs = append(execs, query)
+		return ctx
+	}
 	db := Wrap(sql.OpenDB(conn))
 
 	ctx := context.Background()
@@ -74,42 +86,51 @@ func TestRollback(t *testing.T) {
 		t.Fatal(err)
 	}
 
-	if len(begins) != 1 {
-		t.Errorf("expected 1 begin, got %d", len(begins))
+	if begins != 1 {
+		t.Errorf("expected 1 begin, got %d", begins)
 	}
-	if len(commits) != 1 {
-		t.Errorf("expected 1 commit, got %d", len(commits))
+	if commits != 1 {
+		t.Errorf("expected 1 commit, got %d", commits)
 	}
 	if len(execs) != 5 {
 		t.Errorf("expected 5 execs, got %d", len(execs))
 	}
-	if !insertRegexp.MatchString(execs[0].Query) {
-		t.Errorf("expected insert, got %q", execs[0].Query)
+	if !insertRegexp.MatchString(execs[0]) {
+		t.Errorf("expected insert, got %q", execs[0])
 	}
-	if !savepointRegexp.MatchString(execs[1].Query) {
-		t.Errorf("expected savepoint, got %q", execs[1].Query)
+	if !savepointRegexp.MatchString(execs[1]) {
+		t.Errorf("expected savepoint, got %q", execs[1])
 	}
-	if !insertRegexp.MatchString(execs[2].Query) {
-		t.Errorf("expected insert, got %q", execs[2].Query)
+	if !insertRegexp.MatchString(execs[2]) {
+		t.Errorf("expected insert, got %q", execs[2])
 	}
-	if !rollbackRegexp.MatchString(execs[3].Query) {
-		t.Errorf("expected rollback, got %q", execs[3].Query)
+	if !rollbackRegexp.MatchString(execs[3]) {
+		t.Errorf("expected rollback, got %q", execs[3])
 	}
-	if !insertRegexp.MatchString(execs[4].Query) {
-		t.Errorf("expected insert, got %q", execs[4].Query)
+	if !insertRegexp.MatchString(execs[4]) {
+		t.Errorf("expected insert, got %q", execs[4])
 	}
 }
 
 func TestRelease(t *testing.T) {
 	var (
-		begins  []hooks.BeginInfo
-		commits []hooks.CommitInfo
-		execs   []hooks.ExecInfo
+		begins  int
+		commits int
+		execs   []string
 	)
 	conn := hooks.Wrap(&mockConnector{conn: &mockConn{tx: &mockTx{}}})
-	conn.BeginHook = func(ctx context.Context, info hooks.BeginInfo) { begins = append(begins, info) }
-	conn.CommitHook = func(info hooks.CommitInfo) { commits = append(commits, info) }
-	conn.ExecHook = func(ctx context.Context, info hooks.ExecInfo) { execs = append(execs, info) }
+	conn.BeforeBegin = func(ctx context.Context, opts driver.TxOptions) context.Context {
+		begins++
+		return ctx
+	}
+	conn.BeforeCommit = func(ctx context.Context) context.Context {
+		commits++
+		return ctx
+	}
+	conn.BeforeExec = func(ctx context.Context, query string, args []driver.NamedValue) context.Context {
+		execs = append(execs, query)
+		return ctx
+	}
 	db := Wrap(sql.OpenDB(conn))
 
 	ctx := context.Background()
@@ -134,25 +155,25 @@ func TestRelease(t *testing.T) {
 		t.Fatal(err)
 	}
 
-	if len(begins) != 1 {
-		t.Errorf("expected 1 begin, got %d", len(begins))
+	if begins != 1 {
+		t.Errorf("expected 1 begin, got %d", begins)
 	}
-	if len(commits) != 1 {
-		t.Errorf("expected 1 commit, got %d", len(commits))
+	if commits != 1 {
+		t.Errorf("expected 1 commit, got %d", commits)
 	}
 	if len(execs) != 4 {
 		t.Errorf("expected 4 execs, got %d", len(execs))
 	}
-	if !insertRegexp.MatchString(execs[0].Query) {
-		t.Errorf("expected insert, got %q", execs[0].Query)
+	if !insertRegexp.MatchString(execs[0]) {
+		t.Errorf("expected insert, got %q", execs[0])
 	}
-	if !savepointRegexp.MatchString(execs[1].Query) {
-		t.Errorf("expected savepoint, got %q", execs[1].Query)
+	if !savepointRegexp.MatchString(execs[1]) {
+		t.Errorf("expected savepoint, got %q", execs[1])
 	}
-	if !insertRegexp.MatchString(execs[2].Query) {
-		t.Errorf("expected insert, got %q", execs[2].Query)
+	if !insertRegexp.MatchString(execs[2]) {
+		t.Errorf("expected insert, got %q", execs[2])
 	}
-	if !releaseRegexp.MatchString(execs[3].Query) {
-		t.Errorf("expected release, got %q", execs[3].Query)
+	if !releaseRegexp.MatchString(execs[3]) {
+		t.Errorf("expected release, got %q", execs[3])
 	}
 }
